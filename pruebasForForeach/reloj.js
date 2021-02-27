442a0f34edb1996f155a788be3ff3e8fc4f86a6e
chamocellDeveloper <chamocelDeveloper@gimail.com>
2021-02-27 13:54:40 -0600

    commit colocacion de reloj

---
+++ pruebasForForeach/reloj.js
@@ -0,0 +1,30 @@
+
+const clock = () => {
+    let hour = document.getElementById('hour')
+    let minute = document.getElementById('minute')
+    let seconds = document.getElementById('seconds')
+    let ampm = document.getElementById('ampm')
+
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    const am = 'PM'
+
+    // convert 24 a 12
+
+    if(h > 12){
+        h = h -12
+        const am = 'PM'
+    }
+
+    h = (h < 10) ? '0' + h : h
+    m = (m < 10) ? '0' + m : m
+    s = (s < 10) ? '0' + s : s
+
+    hour.innerHTML = h
+    minute.innerHTML = m
+    seconds.innerHTML = s
+    ampm.innerHTML = am
+}
+
+const interval = setInterval(clock, 1000)
